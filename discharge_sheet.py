53253ca93f10a131065c1ad1edfb114935e0dd7a
muskan1805 <muskan617sharma@gmail.com>
2024-09-22 21:07:23 +0530

    Update discharge_sheet.py

--- discharge_sheet.py
+++ discharge_sheet.py
@@ -102,7 +102,7 @@ if st.button("Reset App"):
 
 # Audio recording component
 wav_audio_data = st_audiorec()
-final_transcription = None
+
 if wav_audio_data:
     file_path = 'save_recorded_audio.wav'
     
@@ -137,15 +137,14 @@ if wav_audio_data:
     # Proceed button
     if st.button("Proceed"):
         # Merge transcription with additional text
-        final_transcription = transcription_text + " " + additional_text.strip()
-        st.session_state.final_transcription = final_transcription
+        st.session_state.final_transcription = transcription_text + " " + additional_text.strip()
         
         st.write("Final Input Data: ", st.session_state.final_transcription)
     
 # st.session_state.final_input_data = final_transcription
 
 
-if final_transcription:
+if st.session_state.final_transcription:
     thread = client.beta.threads.create(
     messages=[
         {
@@ -227,7 +226,7 @@ if final_transcription:
             # Convert the extracted string to a Python dictionary using ast.literal_eval
             investigations_dict = ast.literal_eval(investigations_str)
             st.session_state.investigations_dict = investigations_dict
-            st.write(investigations_dict)
+            #st.write(investigations_dict)
         
             
         except (SyntaxError, ValueError) as e:
@@ -261,7 +260,7 @@ if final_transcription:
             # Load the string as a JSON-compatible dictionary
             advise_dict = json.loads(advise_str)
             st.session_state.advise_dict = advise_dict
-            st.write(advise_dict)
+            #st.write(advise_dict)
             
         except json.JSONDecodeError as e:
             st.write(f"Error parsing dictionary: {e}")
